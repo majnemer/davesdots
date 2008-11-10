d6d3b7714953f3625e654388ca6bf99d12b54bb7
dave <dave@debian.localdomain>
2008-11-10 13:07:17 -0600

    xmonad stuff

---
+++ xmonad.hs
@@ -0,0 +1,16 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run
+import System.IO
+
+main = do
+	xmproc <- spawnPipe "xmobar"
+	xmonad $ defaultConfig {
+		manageHook = manageDocks <+> manageHook defaultConfig,
+		layoutHook = avoidStruts  $  layoutHook defaultConfig,
+		logHook    = dynamicLogWithPP $ xmobarPP {
+					ppOutput = hPutStrLn xmproc,
+					ppTitle = xmobarColor "green" "" . shorten 50
+					}
+		}
