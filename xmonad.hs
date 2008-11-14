3020be4bef266838b4d18f7428920f8a67c59252
Adrian Kreher <akreher2@uiuc.edu>
2008-11-14 12:48:56 -0600

    xmonad.hs: spaces -> tabs, as it should be.

--- xmonad.hs
+++ xmonad.hs
@@ -9,18 +9,18 @@ import XMonad.Util.Run
 import System.IO
 
 myLayoutHook = tiled ||| Mirror tiled ||| Grid ||| Full
-  where
-      -- default tiling algorithm partitions the screen into two panes
-      tiled   = Tall nmaster delta ratio
+	where
+		-- default tiling algorithm partitions the screen into two panes
+		tiled   = Tall nmaster delta ratio
 
-      -- The default number of windows in the master pane
-      nmaster = 1
+		-- The default number of windows in the master pane
+		nmaster = 1
 
-      -- Default proportion of screen occupied by master pane
-      ratio   = 1/2
+		-- Default proportion of screen occupied by master pane
+		ratio   = 1/2
 
-      -- Percent of screen to increment by when resizing panes
-      delta   = 3/100
+		-- Percent of screen to increment by when resizing panes
+		delta   = 3/100
 
 main = do
 	xmproc <- spawnPipe "xmobar"
