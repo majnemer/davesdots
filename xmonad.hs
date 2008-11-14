8a9a03709be393f0c78a08969d73a2867fd22319
Adrian Kreher <akreher2@uiuc.edu>
2008-11-13 18:11:41 -0600

    xmonad.hs: added the useful grid layout to xmonad. This requires the contrib package.

--- xmonad.hs
+++ xmonad.hs
@@ -1,14 +1,32 @@
 import XMonad
+
+import XMonad.Layout
+import XMonad.Layout.Grid
+
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run
 import System.IO
 
+myLayoutHook = tiled ||| Mirror tiled ||| Grid ||| Full
+  where
+      -- default tiling algorithm partitions the screen into two panes
+      tiled   = Tall nmaster delta ratio
+
+      -- The default number of windows in the master pane
+      nmaster = 1
+
+      -- Default proportion of screen occupied by master pane
+      ratio   = 1/2
+
+      -- Percent of screen to increment by when resizing panes
+      delta   = 3/100
+
 main = do
 	xmproc <- spawnPipe "xmobar"
 	xmonad $ defaultConfig {
 		manageHook = manageDocks <+> manageHook defaultConfig,
-		layoutHook = avoidStruts  $  layoutHook defaultConfig,
+		layoutHook = avoidStruts  $  myLayoutHook,
 		logHook    = dynamicLogWithPP $ xmobarPP {
 					ppOutput = hPutStrLn xmproc,
 					ppTitle = xmobarColor "green" ""
